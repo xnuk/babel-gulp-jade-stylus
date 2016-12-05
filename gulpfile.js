7a36dabc6fd43e6f6e608577b161c636eae01717
Xnuk Shuman <admin@xnu.kr>
2016-12-05 17:15:42 +0900

    rename from jade to pug
    + add babelrc

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,53 @@
-var argv        = require('yargs').argv
-var gulp        = require('gulp')
-var jade        = require('gulp-jade')
-var babel       = require('gulp-babel')
-var stylus      = require('gulp-stylus')
-var browserSync = require('browser-sync')
-var reload      = browserSync.reload;
-var sourcemaps = require('gulp-sourcemaps');
-var plumber = require('gulp-plumber');
+const {argv} = require('yargs')
 
-//var dest = argv.dir || '../egoscio.github.io'
-var dest = argv.dir || './index.html'
+const gulp       = require('gulp')
+const pug        = require('gulp-pug')
+const babel      = require('gulp-babel')
+const stylus     = require('gulp-stylus')
+const plumber    = require('gulp-plumber')
+const sourcemaps = require('gulp-sourcemaps')
+
+const browserSync = require('browser-sync')
+const {reload} = browserSync
+
+const babelrc = JSON.parse(require('fs').readFileSync('./.babelrc', 'utf8'))
+
+const dest = argv.dir || './index.html'
 
 gulp.task('default', ['compile'])
 
-gulp.task('compile', ['assets', 'jade', 'babel', 'stylus', 'browser-sync'])
+gulp.task('compile', ['assets', 'pug', 'babel', 'stylus', 'browser-sync'])
 
 
-gulp.task('assets', () => {
-  return gulp.src('src/assets/*')
+gulp.task('assets', () =>
+  gulp.src('src/assets/*')
     .pipe(gulp.dest(dest))
-})
+)
 
-gulp.task('jade', () => {
-  return gulp.src('src/jade/*.jade')
-    .pipe(jade({ pretty: true, doctype: 'html' }))
+gulp.task('pug', () =>
+  gulp.src('src/jade/*.jade')
+    .pipe(pug())
     .pipe(gulp.dest(dest))
-})
+)
 
-gulp.task('babel', () => {
-  return gulp.src('src/babel/*.js')
+gulp.task('babel', () =>
+  gulp.src('src/babel/*.js')
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(babel(babelrc))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(dest))
-})
+)
 
-gulp.task('stylus', () => {
-  return gulp.src('src/stylus/*.styl')
+gulp.task('stylus', () =>
+  gulp.src('src/stylus/*.styl')
     .pipe(sourcemaps.init())
     .pipe(stylus())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(dest))
-})
+)
 
-gulp.task('browser-sync', function() {  
+gulp.task('browser-sync', () => {
   browserSync.init(['./index.html/**.*'], {
     server: {
       baseDir: "./index.html"
@@ -53,10 +56,9 @@ gulp.task('browser-sync', function() {
 })
 
 gulp.task('watch', ['browser-sync'], () => {
-  gulp.watch('index.html/*.js', reload);
+  gulp.watch('index.html/*.js', reload)
   gulp.watch('src/assets/*', ['assets'])
-  gulp.watch('src/jade/*.jade', ['jade'])
+  gulp.watch('src/jade/*.jade', ['pug'])
   gulp.watch('src/babel/*.js', ['babel'])
-  gulp.watch('src/stylus/*.styl', ['stylus'])
   gulp.watch('src/stylus/*.styl', ['stylus'])
 })
